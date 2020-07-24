8bc723a265e89aa7e8dbba2f2287b36ca9eb5e25
Muhammad Ibragimov <mibragimov@hotmail.com>
2020-07-24 17:37:45 +0500

    render ssr

--- pages/index.js
+++ pages/index.js
@@ -216,7 +216,7 @@ Home.defaultProps = {
   i18nNamespaces: ["home"],
 };
 
-export async function getStaticProps() {
+export async function getServerSideProps() {
   const { data } = await Axios.get("https://cov19.cc/report.json");
 
   return {
